8ec8dcb89395ac074d3b41e62714cde3b62beffc
Daniil Akimochkin <okkdanik@yandex.ru>
2025-02-18 13:22:02 +0300

    add video capture functionality

---
+++ detect/camera.py
@@ -0,0 +1,29 @@
+import cv2
+
+def video_capture():
+    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
+    if not cap.isOpened():
+        print("We couldn't open your camera")
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        cv2.imshow('REC...', frame)
+
+        key = cv2.waitKey(25) & 0xFF
+
+        if key == ord('s'):
+            cap.release()
+            cv2.destroyAllWindows()
+            return frame
+        elif key == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+    return None
+
