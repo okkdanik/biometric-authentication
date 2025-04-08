4da074b2ecfc6deb9cd3fe92f20d73734001271c
Daniil Akimochkin <okkdanik@yandex.ru>
2025-04-08 12:52:10 +0300

    refactor(database): add connection pool for Redis client

--- database/redis_cli.py
+++ database/redis_cli.py
@@ -1,20 +1,23 @@
 import redis
 from config.local import redis_conf
 
+connection_pool = redis.ConnectionPool(**redis_conf)
+
 def redis_conn():
-    return redis.Redis(**redis_conf)
+    return redis.Redis(connection_pool=connection_pool)
 
 def check_username(username):
-    r = redis_conn()
-    return r.exists(username) == 0
+    with redis_conn() as r:
+        return r.exists(username) == 0
 
 def add_user(username, encoding):
-    r = redis_conn()
-    r.set(username, encoding)
+    with redis_conn() as r:
+        r.set(username, encoding)
     print(f'\033[92mUser [{username}] has been successfully registered\n\033[0m')
 
 def get_info():
-    r = redis_conn()
-    users = [(user.decode('utf-8'), r.get(user)) for user in r.keys()]
-
-    return users
+    with redis_conn() as r:
+        users = []
+        for key in r.scan_iter():
+            users.append((key.decode('utf-8'), r.get(key)))
+        return users
